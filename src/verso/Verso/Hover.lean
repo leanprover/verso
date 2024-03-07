3e8374f0408d195eafb9428e58471214b162b704
David Thrane Christiansen <david@lean-fro.org>
2024-03-07 14:58:15 +0100

    feat: add an argument parser for code blocks, roles, and directives (#51)
    This will lead to more robust commands and better error messages.

--- src/verso/Verso/Hover.lean
+++ src/verso/Verso/Hover.lean
@@ -4,10 +4,29 @@ namespace Verso.Hover
 
 open Lean Elab
 
+inductive UserHover where
+  /-- A Markdown string to send as the hover -/
+  | markdown (markedString : String)
+  | messageData (messageData : MessageData)
+
+def CustomHover.toString (hover : UserHover) : IO String :=
+  match hover with
+  | .markdown str => pure str
+  | .messageData m => m.toString
+
+instance : Coe String UserHover where
+  coe := .markdown
+
+instance : Coe MessageData UserHover where
+  coe := .messageData
+
 structure CustomHover where
   /-- A Markdown string to send as the hover -/
   markedString : String
-deriving TypeName, Repr
+deriving TypeName
 
-def addCustomHover [Monad m] [MonadInfoTree m] (stx : Syntax) (markdown : String) : m Unit := do
-  pushInfoLeaf <| .ofCustomInfo ⟨stx, .mk <| CustomHover.mk markdown⟩
+def addCustomHover [Monad m] [MonadInfoTree m] [MonadLiftT IO m] (stx : Syntax) (hover : UserHover) : m Unit := do
+  let txt ← match hover with
+    | .markdown str => pure str
+    | .messageData m => m.toString
+  pushInfoLeaf <| .ofCustomInfo ⟨stx, .mk <| CustomHover.mk txt⟩
