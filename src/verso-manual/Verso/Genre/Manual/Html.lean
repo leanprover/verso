3f2619598c0202929a973dded7cb273158f67ae0
David Thrane Christiansen <david@davidchristiansen.dk>
2024-03-21 11:29:22 +0100

    chore: copyright header housekeeping and CI

--- src/verso-manual/Verso/Genre/Manual/Html.lean
+++ src/verso-manual/Verso/Genre/Manual/Html.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2023-2024 Lean FRO LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: David Thrane Christiansen
+-/
+
 import Verso.Output.Html
 
 namespace Verso.Genre.Manual.Html
--- src/verso/Verso.lean
+++ src/verso/Verso.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2023-2024 Lean FRO LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: David Thrane Christiansen
+-/
+
 -- This module serves as the root of the `Verso` library.
 -- Import modules here that should be built as part of the library.
 import Verso.Build
