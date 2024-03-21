cde48ccf31a07ddc62f91409cfc9c58a5f83f703
David Thrane Christiansen <david@davidchristiansen.dk>
2024-03-21 11:17:14 +0100

    chore: library root file for Verso

--- src/verso/Verso.lean
+++ src/verso/Verso.lean
@@ -1,10 +1,24 @@
 -- This module serves as the root of the `Verso` library.
 -- Import modules here that should be built as part of the library.
-import Verso.Basic
+import Verso.Build
+import Verso.Doc
+import Verso.Doc.ArgParse
 import Verso.Doc.Concrete
 import Verso.Doc.Elab
+import Verso.Doc.Elab.ExpanderAttribute
+import Verso.Doc.Elab.InlineString
 import Verso.Doc.Elab.Monad
 import Verso.Doc.Html
 import Verso.Doc.Lsp
-import Verso.Parser
+import Verso.Doc.Suggestion
+import Verso.Doc.TeX
+import Verso.Examples
+import Verso.Hover
+import Verso.Method
+import Verso.Output
 import Verso.Output.Html
+import Verso.Output.TeX
+import Verso.Parser
+import Verso.Parser.Lean
+import Verso.Syntax
+import Verso.SyntaxUtils
