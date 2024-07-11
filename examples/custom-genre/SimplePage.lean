89f3066d1328164328acedf67890a54f34b455d8
David Thrane Christiansen <david@davidchristiansen.dk>
2024-07-11 14:54:39 +0200

    fix: update monad

--- examples/custom-genre/SimplePage.lean
+++ examples/custom-genre/SimplePage.lean
@@ -248,13 +248,14 @@ def render (doc : Part SimplePage) : IO UInt32 := do
     IO.eprintln str
 
   IO.println "Rendering HTML"
+  let (content, _) ← SimplePage.toHtml {logError} context state doc .empty
   let html := {{
     <html>
       <head>
         <title>{{doc.titleString}}</title>
         <meta charset="utf-8"/>
       </head>
-      <body>{{← SimplePage.toHtml {logError} context state doc}}</body>
+      <body>{{ content }}</body>
     </html>
   }}
 
