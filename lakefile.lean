4b3a770cc7ebeb283ff36aa1d32e71136ec6be7d
David Thrane Christiansen <david@davidchristiansen.dk>
2024-08-08 16:32:09 +0200

    chore: reflect the real Verso's lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -1,17 +1,14 @@
 import Lake
 open Lake DSL
 
-require subverso from git "https://github.com/leanprover/subverso.git"@"main"
-require MD4Lean from git "https://github.com/david-christiansen/md4lean"@"parser"
-
 package verso where
 
   -- add package configuration options here
 
 @[default_target]
-lean_lib VersoA where
+lean_lib Verso where
   srcDir := "src/verso"
-  roots := #[`Verso.A]
+  roots := #[`Verso]
 
 @[default_target]
 lean_lib VersoBlog where
