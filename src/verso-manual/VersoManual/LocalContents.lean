fdd9e175efa28d29c305bd82848e79675e9e141c
David Thrane Christiansen <david@davidchristiansen.dk>
2025-02-11 14:48:58 +0100

    Copyright header fix

--- src/verso-manual/VersoManual/LocalContents.lean
+++ src/verso-manual/VersoManual/LocalContents.lean
@@ -1,4 +1,8 @@
-import Lean
+/-
+Copyright (c) 2025 Lean FRO LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: David Thrane Christiansen
+-/
 import Verso
 import VersoManual.Basic
 
