5e49001533eb5ad052ac87cd68c3b7179ad3353c
Sofia Rodrigues <sofia@algebraic.dev>
2025-05-10 18:10:12 +0200

    fix: behavior of SVG tags inside the HTML.

--- src/verso/Verso/Output/Html.lean
+++ src/verso/Verso/Output/Html.lean
@@ -97,9 +97,14 @@ private def voidTags : List String :=
 /--
 Tags for which tag omission rules do not permit omitting the
 closing tag (the XHTML-style `<selfclosing/>` syntax is also invalid
-here)
+here). This list includes all SVG tags, since SVG is XML based and XML requires
+that every tag be explicitly closed.
 
-This was manually constructed by grepping through https://html.spec.whatwg.org/
+This was manually constructed by grepping through
+- https://html.spec.whatwg.org/
+- https://www.w3.org/TR/SVG11/eltindex.html
+- https://www.w3.org/TR/SVGTiny12/elementTable.html
+- https://www.w3.org/TR/SVG2/eltindex.html
 -/
 private def mustClose : List String :=
   ["b", "u", "mark", "bdi", "bdo", "span", "ins", "del", "picture", "iframe",
@@ -110,32 +115,23 @@ private def mustClose : List String :=
    "h2", "h3", "h4", "h5", "h6", "hgroup", "header", "footer", "address", "pre",
    "blockquote", "ol", "ul", "menu", "dl", "figure", "figcaption", "main", "search",
    "div", "a", "em", "strong", "small", "s", "cite", "q", "dfn", "abbr", "ruby",
-   "data", "time", "code", "var", "samp", "kbd", "sub", "sup", "i",
-
-   -- These are the SVGs tags, all of them have must be closed because SVG is expressed
-   -- as XML not as HTML.
-
-  -- This is the source of most of them:
-
-  -- https://www.w3.org/TR/SVG11/eltindex.html,
-  -- https://www.w3.org/TR/SVGTiny12/elementTable.html,
-  -- https://www.w3.org/TR/SVG2/eltindex.htm'
-
-  "altGlyph", "a", "altGlyphDef", "altGlyphItem", "animate", "animateColor",
-  "animateMotion", "animateTransform", "circle", "clipPath", "color-profile",
-  "cursor", "defs", "desc", "ellipse", "feBlend", "feColorMatrix",
-  "feComponentTransfer", "feComposite", "feConvolveMatrix", "feDiffuseLighting",
-  "feDisplacementMap", "feDistantLight", "feFlood", "feFuncA", "feFuncB",
-  "feFuncG", "feFuncR", "feGaussianBlur", "feImage", "feMerge", "feMergeNode",
-  "feMorphology", "feOffset", "fePointLight", "feSpecularLighting", "feSpotLight",
-  "feTile", "feTurbulence", "filter", "font", "font-face", "font-face-format",
-  "font-face-name", "font-face-src", "font-face-uri", "foreignObject", "g",
-  "glyph", "glyphRef", "hkern", "image", "line", "linearGradient", "marker",
-  "mask", "metadata", "missing-glyph", "mpath", "path", "pattern", "polygon",
-  "polyline", "radialGradient", "rect", "script", "set", "stop", "style", "svg",
-  "switch", "symbol", "text", "textPath", "title", "tref", "tspan", "use", "view",
-  "vkern", "audio", "canvas", "discard", "feDropShadow", "iframe", "unknown",
-  "video"]
+   "data", "time", "code", "var", "samp", "kbd", "sub", "sup", "i"] ++
+  -- SVG tags begin here
+  #["altGlyph", "a", "altGlyphDef", "altGlyphItem", "animate", "animateColor",
+   "animateMotion", "animateTransform", "circle", "clipPath", "color-profile",
+   "cursor", "defs", "desc", "ellipse", "feBlend", "feColorMatrix",
+   "feComponentTransfer", "feComposite", "feConvolveMatrix", "feDiffuseLighting",
+   "feDisplacementMap", "feDistantLight", "feFlood", "feFuncA", "feFuncB",
+   "feFuncG", "feFuncR", "feGaussianBlur", "feImage", "feMerge", "feMergeNode",
+   "feMorphology", "feOffset", "fePointLight", "feSpecularLighting", "feSpotLight",
+   "feTile", "feTurbulence", "filter", "font", "font-face", "font-face-format",
+   "font-face-name", "font-face-src", "font-face-uri", "foreignObject", "g",
+   "glyph", "glyphRef", "hkern", "image", "line", "linearGradient", "marker",
+   "mask", "metadata", "missing-glyph", "mpath", "path", "pattern", "polygon",
+   "polyline", "radialGradient", "rect", "script", "set", "stop", "style", "svg",
+   "switch", "symbol", "text", "textPath", "title", "tref", "tspan", "use", "view",
+   "vkern", "audio", "canvas", "discard", "feDropShadow", "iframe", "unknown",
+   "video"]
 
 /--
   Tags to break the line after without risking weird results
