08fd7641574c8fe1dc31c1e9a13dfe74b86218a1
David Thrane Christiansen <david@lean-fro.org>
2025-05-23 17:08:06 +0200

    fix: external code rebuilds and hints (#404)
    Prevents spurious rebuilds from subprocesses and repairs broken hint formatting.

--- src/verso-blog/VersoBlog/LiterateLeanPage.lean
+++ src/verso-blog/VersoBlog/LiterateLeanPage.lean
@@ -48,6 +48,7 @@ def loadModuleContent
   let lakeVars :=
     #["LAKE", "LAKE_HOME", "LAKE_PKG_URL_MAP",
       "LEAN_SYSROOT", "LEAN_AR", "LEAN_PATH", "LEAN_SRC_PATH",
+      "LEAN_GITHASH",
       "ELAN_TOOLCHAIN", "DYLD_LIBRARY_PATH", "LD_LIBRARY_PATH"]
 
 
@@ -133,6 +134,7 @@ def Helper.fromModule
   let lakeVars :=
     #["LAKE", "LAKE_HOME", "LAKE_PKG_URL_MAP",
       "LEAN_SYSROOT", "LEAN_AR", "LEAN_PATH", "LEAN_SRC_PATH",
+      "LEAN_GITHASH",
       "ELAN_TOOLCHAIN", "DYLD_LIBRARY_PATH", "LD_LIBRARY_PATH"]
 
   let cmd := "elan"
