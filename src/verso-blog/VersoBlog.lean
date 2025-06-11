905cb8ab6d422b11f079a9fb06e3060ce5a28b82
Pim Otte <p.j.otte@uu.nl>
2025-06-11 09:31:54 +0200

    Whitespace

--- src/verso-blog/VersoBlog.lean
+++ src/verso-blog/VersoBlog.lean
@@ -783,9 +783,8 @@ private def filterString (p : Char → Bool) (str : String) : String := Id.run <
   pure out
 
 open Template in
-def blogMain (theme : Theme) (site : Site) (relativizeUrls := true)
-    (linkTargets := {}) (options : List String)
-    (components : Components := by exact %registered_components) :
+def blogMain (theme : Theme) (site : Site) (relativizeUrls := true) (linkTargets := {})
+    (options : List String) (components : Components := by exact %registered_components) :
     IO UInt32 := do
   let hasError ← IO.mkRef false
   let logError msg := do hasError.set true; IO.eprintln msg
