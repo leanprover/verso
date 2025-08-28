a916c888f6507dc7644d18cdfc7a2e914f19dea9
Jason Reed <jcreed@gmail.com>
2025-08-28 11:28:11 -0400

    A trivial integration test

--- src/tests/Tests/ManualTeX.lean
+++ src/tests/Tests/ManualTeX.lean
@@ -1,6 +1,25 @@
+import Verso
+import VersoManual
+
 namespace Verso.ManualTexTest
 
-def run (input : String) : IO String :=
+open Verso Genre Manual
+
+def testTexMain : IO Unit := open Verso Genre Manual in do
+ let logError (msg : String) := IO.eprintln msg
+ let cfg : Config := {
+   destination := "/tmp/_out",
+   emitTeX := true,
+   emitHtmlMulti := false,
+   }
+ let part : Doc.Part Manual := Doc.Part.mk #[] "title" none #[] #[]
+ let exts : ExtensionImpls := (ExtensionImpls.fromLists [] [])
+ let z := ReaderT.run (emitTeX logError cfg part) exts
+ _ ← z
+ return
+
+def run (input : String) : IO String := do
+  testTexMain
   pure s!"{input}"
 
 end Verso.ManualTexTest
