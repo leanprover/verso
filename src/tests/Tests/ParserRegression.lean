0734e5ad642ffdc0e98c77f5122c7e2050a70852
David Thrane Christiansen <david@lean-fro.org>
2025-09-21 13:02:16 +0200

    fix: add '%%%' as a token when parsing metadata blocks (#532)
    Fixes #531

---
+++ src/tests/Tests/ParserRegression.lean
@@ -0,0 +1,23 @@
+/-
+Copyright (c) 2025 Lean FRO LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Jason Reed
+-/
+import Verso
+import VersoManual
+
+/-
+This is a test for https://github.com/leanprover/verso/issues/531
+-/
+
+open Verso Genre Manual
+
+#docs (Manual) sample_part "Title of the Doc" :=
+:::::::
+
+%%%
+shortTitle := "ShortTitle"
+authors := ["Harry Q. Bovik"]
+%%%
+
+:::::::
