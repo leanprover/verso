d34ce62cbcdbabe124f55075c6d5f4f0daa2df08
Robert J. Simmons <442315+robsimmons@users.noreply.github.com>
2025-10-13 17:00:34 -0400

    test: add back in (and add additional) reference and footnote tests (#568)
    In the Examples -> BasicTest switch I accidentally dropped some tests,
    this adds them back and also adds some others

--- src/tests/Tests/Basic.lean
+++ src/tests/Tests/Basic.lean
@@ -276,3 +276,128 @@ info: Verso.Doc.Part.mk
 #### Sub^3section
 
 :::::::
+
+
+/- ----- -/
+
+#docs (.none) regularLink "Regular link" :=
+:::::::
+Here's [a link](http://example.com)
+:::::::
+/--
+info: Verso.Doc.Part.mk
+  #[Verso.Doc.Inline.text "Regular link"]
+  "Regular link"
+  none
+  #[Verso.Doc.Block.para
+      #[Verso.Doc.Inline.text "Here's ",
+        Verso.Doc.Inline.link #[(Verso.Doc.Inline.text "a link")] "http://example.com"]]
+  #[]
+-/
+#guard_msgs in
+  #eval regularLink
+
+
+/- ----- -/
+
+#docs (.none) refLink "Ref link" :=
+:::::::
+Here's [a link][to here]
+
+[to here]: http://example.com
+:::::::
+/--
+info: Verso.Doc.Part.mk
+  #[Verso.Doc.Inline.text "Ref link"]
+  "Ref link"
+  none
+  #[Verso.Doc.Block.para
+      #[Verso.Doc.Inline.text "Here's ",
+        Verso.Doc.Inline.link #[(Verso.Doc.Inline.text "a link")] "http://example.com"]]
+  #[]
+-/
+#guard_msgs in
+  #eval refLink
+
+
+/- ----- -/
+
+#docs (.none) noteLink "Footnote" :=
+:::::::
+Here's something that needs context[^note]!
+
+[^note]: The footnote text
+:::::::
+/--
+info: Verso.Doc.Part.mk
+  #[Verso.Doc.Inline.text "Footnote"]
+  "Footnote"
+  none
+  #[Verso.Doc.Block.para
+      #[Verso.Doc.Inline.text "Here's something that needs context",
+        Verso.Doc.Inline.footnote "note" #[(Verso.Doc.Inline.text "The footnote text")], Verso.Doc.Inline.text "!"]]
+  #[]
+-/
+#guard_msgs in
+  #eval noteLink
+
+
+/- ----- -/
+
+instance : BEq (Doc.Part Doc.Genre.none) where
+  beq x y := BEq.beq (self := Doc.instBEqPart) x y
+
+#docs (.none) refAndLink "Ref/link ordering" :=
+:::::::
+[to here]: http://example.com
+
+Here's [a link][to here][^note]!
+
+[^note]: The footnote text
+:::::::
+/--
+info: Verso.Doc.Part.mk
+  #[Verso.Doc.Inline.text "Ref/link ordering"]
+  "Ref/link ordering"
+  none
+  #[Verso.Doc.Block.para
+      #[Verso.Doc.Inline.text "Here's ", Verso.Doc.Inline.link #[(Verso.Doc.Inline.text "a link")] "http://example.com",
+        Verso.Doc.Inline.footnote "note" #[(Verso.Doc.Inline.text "The footnote text")], Verso.Doc.Inline.text "!"]]
+  #[]
+-/
+#guard_msgs in
+  #eval refAndLink
+
+#docs (.none) refAndLink2 "Ref/link ordering" :=
+:::::::
+Here's [a link][to here][^note]!
+
+[to here]: http://example.com
+[^note]: The footnote text
+:::::::
+
+#docs (.none) refAndLink3 "Ref/link ordering" :=
+:::::::
+[to here]: http://example.com
+[^note]: The footnote text
+
+Here's [a link][to here][^note]!
+:::::::
+
+#docs (.none) refAndLink4 "Ref/link ordering" :=
+:::::::
+[^note]: The footnote text
+
+Here's [a link][to here][^note]!
+
+[to here]: http://example.com
+:::::::
+
+/-- info: true -/
+#guard_msgs in #eval refAndLink == refAndLink2
+
+/-- info: true -/
+#guard_msgs in #eval refAndLink == refAndLink3
+
+/-- info: true -/
+#guard_msgs in #eval refAndLink == refAndLink4
