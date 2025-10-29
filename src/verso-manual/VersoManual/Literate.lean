3eb6336ea8fad235aa93e5662f92f8a44d929e38
Rob Simmons <rob@lean-fro.org>
2025-10-29 13:48:48 -0400

    Grab a moduleGenreElabContext for refman to use

--- src/verso-manual/VersoManual/Literate.lean
+++ src/verso-manual/VersoManual/Literate.lean
@@ -62,6 +62,12 @@ instance : LoadLiterate Manual where
 
   docstringPart lvl title contents := .other (Block.literateDocstringPart lvl) (#[.para title] ++ contents)
 
+def moduleGenreElabContext [Monad m] [MonadRef m] [MonadQuotation m] : m Doc.Elab.DocElabContext := do
+  let genre ← ``(Manual)
+  let g := Expr.const ``Manual []
+  return ⟨genre, g⟩
+
+
 open Lean.Doc.Syntax
 open Verso.Doc Elab Concrete
 open Lean.Elab Command Term
@@ -73,10 +79,9 @@ def getModuleWithDocs (path : StrLit) (mod : Ident) (title : StrLit) : PartElabM
   let titleString := inlinesToString (← getEnv) titleParts
   let initState : PartElabM.State := .init (.node .none nullKind titleParts)
 
-  let genre ← ``(Manual)
   let g := Expr.const ``Manual []
 
-  let (titleTerm, _st) ← DocElabM.run ⟨genre, g⟩ {} initState <| do
+  let (titleTerm, _st) ← DocElabM.run (← moduleGenreElabContext) {} initState <| do
     titleParts.mapM (elabInline ⟨·⟩)
 
   let modJson ← withTraceNode `verso.blog.literate.loadMod (fun _ => pure m!"Loading '{mod}' in '{path}'") <|
