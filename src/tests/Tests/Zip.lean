813319c27bf6e2455001baf61dc2459f3c784ac1
David Thrane Christiansen <david@davidchristiansen.dk>
2025-10-31 08:17:19 +0100

    chore: headers

--- src/tests/Tests/Zip.lean
+++ src/tests/Tests/Zip.lean
@@ -1,8 +1,12 @@
+/-
+Copyright (c) 2025 Lean FRO LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: David Thrane Christiansen
+-/
+
 import VersoUtil.Zip
 
 open Verso.Zip
-
-
 
 def testExtract (files : Array (String × ByteArray)) (method : CompressionMethod) : IO Unit := do
   IO.FS.withTempDir fun dir => do
