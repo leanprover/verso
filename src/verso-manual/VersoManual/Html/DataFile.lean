bf1830db339fc2674f700e55467c5e5b0dc70cda
David Thrane Christiansen <david@davidchristiansen.dk>
2025-11-06 10:45:59 +0100

    chore: missing headers

--- src/verso-manual/VersoManual/Html/DataFile.lean
+++ src/verso-manual/VersoManual/Html/DataFile.lean
@@ -1,9 +1,14 @@
+/-
+Copyright (c) 2025 Lean FRO LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: David Thrane Christiansen
+-/
+
 module
 import Lean.Data.Json
 public import Lean.Data.Json.FromToJson
 import Verso.BEq
 import VersoUtil.BinFiles.Z85
-
 
 namespace Verso.Genre.Manual
 open Lean
