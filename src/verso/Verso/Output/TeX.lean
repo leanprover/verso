9a27ddaa529c7f7619a76bdbfb5f1ae538a67d75
David Thrane Christiansen <david@davidchristiansen.dk>
2026-01-23 17:52:57 +0100

    dead code removal

--- src/verso/Verso/Output/TeX.lean
+++ src/verso/Verso/Output/TeX.lean
@@ -52,24 +52,6 @@ namespace TeX
 /-- The empty TeX document -/
 public def empty : TeX := .seq #[]
 
-/-- Checks whether the document is equivalent to the empty string. -/
-public partial def isEmpty : TeX → Bool
-  | .seq xs => xs.all isEmpty
-  | .paragraphBreak => false
-  | .environment .. => false
-  | .command .. => false
-  | .text s => s.isEmpty
-  | .raw s => s.isEmpty
-
-/-- Checks whether the document contains only whitespace. -/
-public partial def isWhitespace : TeX → Bool
-  | .seq xs => xs.all isWhitespace
-  | .paragraphBreak => true
-  | .environment .. => false
-  | .command .. => false
-  | .text s => s.all Char.isWhitespace
-  | .raw s => s.all Char.isWhitespace
-
 /-- Converts a TeX document to a string to be processed by LaTeX -/
 public partial def asString (doc : TeX) : String :=
   match doc with
