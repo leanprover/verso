998367f97cd0e646cd8caeb0fb03a29d67b63ae2
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-01-28 21:36:10 +0100

    doc: Add changelog to users guide.
    Co-authored-by: David Thrane Christiansen <david@davidchristiansen.dk>

---
+++ doc/UsersGuide/Releases/v4_28_0.lean
@@ -0,0 +1,21 @@
+/-
+Copyright (c) 2026 Lean FRO LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Emilio J. Gallego Arias
+-/
+
+import VersoManual
+
+open Verso.Genre
+
+-- To allow ```` below
+set_option linter.verso.markup.codeBlock false
+
+#doc (Manual) "Verso 4.28.0 (unreleased)" =>
+%%%
+tag := "release-v4.28.0"
+file := "v4.28.0"
+%%%
+
+* Add Release Notes / Changelog to Verso Users guide (@ejgallego, #708)
+
