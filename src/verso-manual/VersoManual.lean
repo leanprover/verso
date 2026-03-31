843314c13c702fbfbcc73aa4c77c1697cb3ae455
Alok Singh <alokbeniwal@gmail.com>
2026-03-30 22:01:02 -0700

    Revert "feat: add figure/image block directive to VersoManual"
    This reverts commit 1018d0cf0931105f725450f646081bc2d93b2a67.

--- src/verso-manual/VersoManual.lean
+++ src/verso-manual/VersoManual.lean
@@ -42,7 +42,6 @@ import VersoManual.Literate
 import VersoManual.Marginalia
 import VersoManual.Bibliography
 import VersoManual.Table
-import VersoManual.Figure
 
 open Lean (Name NameMap Json ToJson FromJson quote)
 
