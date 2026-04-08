a1f903ceddfdcc8e062e2ae0ca4bfb0646289fcd
Kim Morrison <kim@tqft.net>
2026-04-08 10:42:38 +1000

    add comment

--- src/verso-manual/VersoManual/WebAssets.lean
+++ src/verso-manual/VersoManual/WebAssets.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: David Thrane Christiansen
 -/
 module
-public meta import Init.System.FilePath
+public meta import Init.System.FilePath -- Temporary workaround for https://github.com/leanprover/lean4/issues/13310
 namespace Verso.Genre.Manual
 
 public def find.js := include_str "../../../static-web/find.js"
