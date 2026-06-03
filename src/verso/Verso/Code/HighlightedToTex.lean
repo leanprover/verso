12aa45102c1a23a6a98aff68dfe6fda9321daa40
David Thrane Christiansen <david@davidchristiansen.dk>
2026-06-03 06:12:11 +0200

    chore: adapt to SubVerso lexical(ish) tokens
    This adapts Verso to https://github.com/leanprover/subverso/pull/211,
    which adds a number of lexical rules to the highlighting in addition
    to the pre-existing semantic rules, allowing more theme flexibility
    downstream.

--- src/verso/Verso/Code/HighlightedToTex.lean
+++ src/verso/Verso/Code/HighlightedToTex.lean
@@ -37,6 +37,16 @@ public def highlightToken : String → Token.Kind → TeX
 | c, .moduleName _ => .raw c
 | c, .levelOp _ => .raw c
 | c, .withType _ => .raw c
+| c, .delim .. => .raw c
+| c, .wildcard .. => .raw c
+| c, .num .. => .raw c
+| c, .char .. => .raw c
+| c, .lineComment => .raw c
+| c, .blockComment => .raw c
+| c, .commentDelim => .raw c
+| c, .operator .. => .raw c
+| c, .bracket .. => .raw c
+| c, .separator .. => .raw c
 | c, .unknown => .raw c
 
 defmethod Highlighting.Token.toVerbatimTeX (t : Highlighting.Token) (lineBreaks : Bool := false) : Verso.Output.TeX :=
