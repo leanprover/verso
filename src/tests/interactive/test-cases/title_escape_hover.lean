a9ae013f9500da3bd9ec754d7043414f3bd1ee79
David Thrane Christiansen <david@davidchristiansen.dk>
2026-06-16 14:37:23 +0200

    make tests more reliable

--- src/tests/interactive/test-cases/title_escape_hover.lean
+++ src/tests/interactive/test-cases/title_escape_hover.lean
@@ -4,6 +4,7 @@ import VersoManual
 open Verso Genre Manual InlineLean
 
 #doc (Manual) "T \" {lean}`Nat`" =>
+                         --^ sync
                          --^ textDocument/hover
 
 Body paragraph.
