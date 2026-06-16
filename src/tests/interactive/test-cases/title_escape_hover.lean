ef83d0726a4db9ae527450bb05bacfe1d1b1bea7
David Thrane Christiansen <david@davidchristiansen.dk>
2026-06-16 14:14:03 +0200

    fix: parse decoded string literals with accurate decoded positions
    This fixes an issue where syntax resulting from parsed string literals
    would have bogus position information, which got in the way of some
    LSP features. Even worse, if this syntax was used in a point of
    interest, it panicked (after the merge of #771).

---
+++ src/tests/interactive/test-cases/title_escape_hover.lean
@@ -0,0 +1,9 @@
+import Verso
+import VersoManual
+
+open Verso Genre Manual InlineLean
+
+#doc (Manual) "T \" {lean}`Nat`" =>
+                         --^ textDocument/hover
+
+Body paragraph.
---
+++ src/tests/interactive/test-cases/title_unicode_escape_hover.lean
@@ -0,0 +1,8 @@
+import Verso
+import VersoManual
+open Verso Genre Manual InlineLean
+
+#doc (Manual) "\u007B\u006C\u0065\u0061\u006E\u007D\u0060\u004E\u0061\u0074\u0060" =>
+                                                          --^ textDocument/hover
+
+Body.
