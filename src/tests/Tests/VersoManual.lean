f95a2acf33feea679c6d1e111f7de380cc669015
Robert J. Simmons <442315+robsimmons@users.noreply.github.com>
2026-07-07 19:22:51 -0400

    chore: fix nightly, don't skip sections regression (#911)
    Because we're importing a non-module file, move VersoManual.lean back
    off the module system

--- src/tests/Tests/VersoManual.lean
+++ src/tests/Tests/VersoManual.lean
@@ -3,11 +3,10 @@ Copyright (c) 2025 Lean FRO LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author: David Thrane Christiansen
 -/
-module
-public import Tests.VersoManual.Docstring
-public import Tests.VersoManual.Html
-public import Tests.VersoManual.Html.SoftHyphenate
-public import Tests.VersoManual.License
-public import Tests.VersoManual.Markdown
-public import Tests.VersoManual.WordCount
-public section
+import Tests.VersoManual.Docstring
+import Tests.VersoManual.Html
+import Tests.VersoManual.Html.SoftHyphenate
+import Tests.VersoManual.License
+import Tests.VersoManual.Markdown
+import Tests.VersoManual.Sections
+import Tests.VersoManual.WordCount
