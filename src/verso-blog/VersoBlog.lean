b41b6c68825ed0457fd9bea1bfac4eeac1444757
Rob Simmons <rob@lean-fro.org>
2026-07-08 16:07:08 -0400

    Fix busted diff

--- src/verso-blog/VersoBlog.lean
+++ src/verso-blog/VersoBlog.lean
@@ -7,7 +7,6 @@ Author: David Thrane Christiansen
 import SubVerso.Highlighting
 import SubVerso.Examples
 
-<<<<<<< Updated upstream
 import VersoBlog.Basic
 import VersoBlog.LiterateLeanPage
 import VersoBlog.LiterateModuleDocs
@@ -22,10 +21,6 @@ import Verso.Doc.Lsp
 import Verso.Doc.Suggestion
 import Verso.Hover
 import Verso.WithoutAsync
-=======
-#check Verso.Genre.Blog.defaultPostName
-
->>>>>>> Stashed changes
 open Verso.Output Html
 
 namespace Verso.Genre.Blog
