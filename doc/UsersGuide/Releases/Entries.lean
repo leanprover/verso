feed1bd538268e4e882b86eaaed47c47964b331a
David Thrane Christiansen <david@davidchristiansen.dk>
2026-09-15 12:07:14 +0200

    fix: update release notes for widget post release

--- doc/UsersGuide/Releases/Entries.lean
+++ doc/UsersGuide/Releases/Entries.lean
@@ -25,4 +25,5 @@ public import UsersGuide.Releases.Entries.ReleaseNotesChapter
 public import UsersGuide.Releases.Entries.RoleDiagnostics
 public import UsersGuide.Releases.Entries.SearchPriority
 public import UsersGuide.Releases.Entries.TestFramework
+public import UsersGuide.Releases.Entries.TestWidget
 public import UsersGuide.Releases.Entries.VersionedReleaseNotes
